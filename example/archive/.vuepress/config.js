05ccf48ccdaf7743082cb4dfaf6bd24cdd68bb4c
ocavue <ocavue@gmail.com>
2019-04-23 04:05:22 +0800

    feat: Use the lastest theme version in the example

--- example/archive/.vuepress/config.js
+++ example/archive/.vuepress/config.js
@@ -1,11 +1,6 @@
 module.exports = {
     title: "Blogue Example",
     description: "Just playing around",
-    base: "/",
     dest: "dist",
-    serviceWorker: false,
     theme: "blogue",
-    themeConfig: {
-        debug: false,
-    },
 }
