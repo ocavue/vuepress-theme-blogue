c337a065ec254d12a41936b84b9f1c01a9cc2556
ocavue <ocavue@gmail.com>
2019-05-18 05:22:39 +0800

    feat: Automatically highlight TOC link when page scrolls

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports = {
     plugins: {
         "@vuepress/blog": {},
+        "@vuepress/plugin-active-header-links": {
+            sidebarLinkSelector: '.toc__item',
+            headerTopOffset: 56,
+        },
     },
     extendPageData ($page) {
         if ($page.path === '/') {
