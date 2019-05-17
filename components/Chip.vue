3fe0efc17e97733ce2740b69ab88cb3c85050b66
ocavue <ocavue@gmail.com>
2019-05-18 05:49:43 +0800

    feat: Update tags page style

---
+++ components/Chip.vue
@@ -0,0 +1,37 @@
+<template>
+    <router-link
+        :to="to"
+        class="chip"
+    >
+        {{ name }}
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: "Chip",
+    props: ["name", "to"],
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../styles/theme';
+
+.chip {
+    border: 1px $premier-color solid;
+    border-radius: 3em;
+    text-align: center;
+    min-width: 1.5em;
+    padding: 0.5em 1em;
+    margin: 0.5em 0.4em;
+    text-decoration: none;
+    color: $premier-color;
+    transition : background-color 100ms, color 100ms;
+
+    &:hover {
+        background-color: $premier-color;
+        color: #ffffff;
+    }
+}
+</style>
+
--- layouts/Tags.vue
+++ layouts/Tags.vue
@@ -2,27 +2,26 @@
     <div class="tags">
         <h1 class="tags--title">Tags</h1>
         <div class="tags-links">
-            <router-link
+            <Chip
                 v-for="(tag, index) in this.$tags.list"
                 :key="index"
                 :to="tag.path"
-                class="tags-links--item"
-            >
-                {{ tag.name }}
-            </router-link>
+                :name="tag.name"
+            />
         </div>
     </div>
 </template>
 
 <script>
+import Chip from "../components/Chip"
+
 export default {
     name: "Tags",
+    components: { Chip },
 }
 </script>
 
 <style lang="stylus">
-@import '../styles/theme';
-
 .tags {
     display: flex;
     flex-direction: column;
@@ -32,18 +31,5 @@ export default {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-}
-
-.tags-links--item {
-    color: $premier-color;
-    padding-bottom: 4px;
-    padding-top: 4px;
-    margin-right: 16px;
-    font-size: 1.2em;
-    color: #5a5a5a;
-
-    &:hover {
-        color: $premier-color;
-    }
 }
 </style>
